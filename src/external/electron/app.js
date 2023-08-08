b76ba8e6635008028ed4c0c37d4533d94ed84741
d.tintore <dario.tintore2@gmail.com>
2023-08-08 17:41:43 +0200

    FEAT: Aggiornato TestSmellDetector

--- src/external/electron/app.js
+++ src/external/electron/app.js
@@ -80,6 +80,7 @@ app.on('activate', function () {
 ipcMain.on('compile',async (event, data) => {
   let result = await refactoring_service.doCompile(data)
   result.testResult = utils.cleanSuccessResponse(result.testResult)
+  console.log(result)
   if(result.success)
     result.smellResult = utils.removeIgnoredSmells(result.smellResult, data[1]);
   console.log(result);
